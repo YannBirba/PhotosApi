826ef17d0ef350371a7d45f5934dd0b93b702b67
Yann Birba <birbayann@gmail.com>
2022-08-21 11:11:24 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
 
-    'allowed_origins' => ['localhost', 'http://localhost:3000'],
+    'allowed_origins' => ['localhost', 'http://localhost:3000', 'http://localhost:5173', 'http://localhost:4173'],
 
     'allowed_origins_patterns' => [],
 
